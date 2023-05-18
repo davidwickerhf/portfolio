ac8075fc3c56cf321aec3c6a10d08d53b3998b6c
David Wicker <davidwickerhf@gmail.com>
2023-05-18 12:06:50 +0200

    Fix before override

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,9 +3,7 @@ import '$lib/i18n'; // Import to initialize. Important :)
 import { locale, waitLocale } from 'svelte-i18n';
 import type { LayoutLoad } from './$types';
 
-console.log('Loaded Servers');
 export const load: LayoutLoad = async () => {
-	console.log('Called load function');
 	if (browser) {
 		console.log('set browser locale');
 		locale.set(window.navigator.language);
